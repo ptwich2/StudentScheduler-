90fafef0fe52f5012cba9833ed24ad77d1bb9518
amelikh2 <amelik2@uic.edu>
2013-11-13 12:04:15 -0600

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,6 +12,15 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->tabWidget->setCurrentIndex(0);
+    
+    QPushButton *okButton = ui->holidayChanges->button(QDialogButtonBox::Ok);
+    connect(okButton, SIGNAL(clicked()), this, SLOT(okBox()));
+
+    QPushButton *resetButton = ui->holidayChanges->button(QDialogButtonBox::Reset);
+    connect(resetButton, SIGNAL(clicked()), this, SLOT(resetBox()));
+
+    ui->holidayStart->setDate(QDate::currentDate());
+    ui->holidayEnd->setDate(QDate::currentDate());
 }
 
 MainWindow::~MainWindow()
@@ -39,4 +48,20 @@ void MainWindow::on_createButton_click()
     //cout << dateEnd;
     //cout << nameSemesterEdit;
 
+}
+
+void MainWindow::okBox()
+{
+    QString holidayNameContents = ui->holidayName->text();
+    QString holidayCommentsContents = ui->holidayComments->toPlainText();
+    QString holidayStartDate = ui->holidayStart->date().toString();
+    QString holidayEndDate = ui->holidayEnd->date().toString();
+}
+
+void MainWindow::resetBox()
+{
+    ui->holidayName->clear();
+    ui->holidayComments->clear();
+    ui->holidayStart->setDate(QDate::currentDate());
+    ui->holidayEnd->setDate(QDate::currentDate());
 }
