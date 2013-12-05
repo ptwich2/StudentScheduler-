54a384be666e8dfd5bc6c22ef349d45540651af3
Saleha Amreen <samree2@uic.edu>
2013-12-05 12:14:06 -0600

    resolved conflicts

--- welcomepage.cpp
+++ welcomepage.cpp
@@ -101,11 +101,19 @@ void WelcomePage::doThis1(MyNetwork *myPost){
 
 void WelcomePage::on_forgetPassword_clicked()
 {
-    ui->signInBox->hide();
-    ui->creditsBox->hide();
-    ui->createAnAccountBox->hide();
+//    ui->signInBox->hide();
+//    ui->creditsBox->hide();
+//    ui->createAnAccountBox->hide();
 
-    ui->forgetBox->show();
+//    ui->forgetBox->show();
+
+    MainWindow mainWindow;
+  //  mainWindow.setUserID(theUserID.toString());
+  //  mainWindow.setGlobalObject(myPost->theResponse);
+    mainWindow.setHiUserText();
+    mainWindow.show();
+    mainWindow.setFixedSize(mainWindow.size());
+    mainWindow.exec();
 }
 
 void WelcomePage::on_createAnAccount_clicked()
