2773e8f7b7236b48125ba85eb67acd1a5aa6304d
Andrey Melikhov <amelikh2@gmail.com>
2013-12-06 20:41:34 -0600

    no message

---
+++ Schedule.mm
@@ -0,0 +1,126 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Schedule" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1380249058428"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<font SIZE="14"/>
+<node TEXT="Create Semester/Quarter" POSITION="right" ID="ID_307900494" CREATED="1380161776561" MODIFIED="1380249058429">
+<icon BUILTIN="pencil"/>
+<edge COLOR="#ff0000"/>
+<font SIZE="14"/>
+<node TEXT="Add/Edit Name" ID="ID_1147074339" CREATED="1380165488991" MODIFIED="1380249058452">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add Start Date" ID="ID_1634663548" CREATED="1380165479017" MODIFIED="1380249058450">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add End Date" ID="ID_260302948" CREATED="1380165484288" MODIFIED="1380249058451">
+<font SIZE="14"/>
+</node>
+<node TEXT="Classes" ID="ID_231066890" CREATED="1380161954988" MODIFIED="1380249058430">
+<font SIZE="14"/>
+<node TEXT="Add Classes" ID="ID_129263024" CREATED="1380162660461" MODIFIED="1380249058441">
+<font SIZE="14"/>
+<node TEXT="Add University" ID="ID_348466581" CREATED="1380162719454" MODIFIED="1380249058441">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add Start Date" ID="ID_1648162679" CREATED="1380162731764" MODIFIED="1380249058443">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add End Date" ID="ID_232821773" CREATED="1380162742564" MODIFIED="1380249058444">
+<font SIZE="14"/>
+</node>
+<node TEXT="Customize Class" ID="ID_937670542" CREATED="1380161975893" MODIFIED="1380249271466">
+<font SIZE="14"/>
+<node TEXT="Class Type" ID="ID_1806576625" CREATED="1380247090892" MODIFIED="1380249058433">
+<font SIZE="14"/>
+<node TEXT="Lab" ID="ID_788589487" CREATED="1380247861682" MODIFIED="1380249058434">
+<font SIZE="14"/>
+</node>
+<node TEXT="Lecture" ID="ID_322287700" CREATED="1380247865706" MODIFIED="1380249058435">
+<font SIZE="14"/>
+</node>
+<node TEXT="Discussion" ID="ID_1500930413" CREATED="1380247870706" MODIFIED="1380249058436">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Add Building" ID="ID_876759831" CREATED="1380247335106" MODIFIED="1380249058437">
+<font SIZE="14"/>
+<node TEXT="Add Room Number" ID="ID_1501450497" CREATED="1380247755946" MODIFIED="1380249058438">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Add Time of Class" ID="ID_171377487" CREATED="1380248671337" MODIFIED="1380249058439">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add Days" ID="ID_478085817" CREATED="1380247614521" MODIFIED="1380249058440">
+<font SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Add Holiday" ID="ID_1939366682" CREATED="1380248984483" MODIFIED="1380249058446">
+<font SIZE="14"/>
+<node TEXT="Holiday 1" ID="ID_1821725197" CREATED="1380249033550" MODIFIED="1380249058447">
+<font SIZE="14"/>
+</node>
+<node TEXT="Vacation 2" ID="ID_1585016409" CREATED="1380249037381" MODIFIED="1380249058448">
+<font SIZE="14"/>
+</node>
+<node TEXT="Holiday X" ID="ID_1582254139" CREATED="1380249041749" MODIFIED="1380249058424">
+<font SIZE="14"/>
+</node>
+<node TEXT="Add/Edit" ID="ID_1408954361" CREATED="1380249044149" MODIFIED="1380249058449">
+<font SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
